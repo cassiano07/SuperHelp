bb1bb03bf77f518da92ff0efe48a58b81906ca00
cassiano07 <57646816+cassiano07@users.noreply.github.com>
2022-12-09 18:19:50 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+<div id="example-option-2" class="install-ok">
+  <button>
+    Install my add-on
+  </button>
+  <script>
+    document.querySelector("#example-option-2 > button").onclick = () => {
+      window.location = "https://example.com/path/to/extension.xpi";
+    };
+  </script>
+</div>
